5409856c46e31ab3b8b55ddf3c735d0b21ff4fd2
Prakash Gurung <makkhay@gmail.com>
2016-09-26 16:43:58 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,320 +1,315 @@
-/**
- * POINTERS TUTORIAL
- * CREDIT: tutorials point c++ pointer tutorials
- * http://www.tutorialspoint.com/cplusplus/cpp_pointers.htm
- */
 #include <iostream>
 
 #define MAX 3
 using namespace std;
 
 namespace example1 {
-    void example() {
-        //pointers basic 1
-        //TODO 1 - create var1 of type int
-        //TODO 1 - create a character array var2 that can hold 10 elements
+void example() {
+  //pointers basic 1
+  //TODO - create var1 of type int
+  //TODO - create a character array var2 that can hold 10 elements
+  int var1;
+  char var2[10];
 
+  cout << "Size of var1 variable: ";
+  cout << sizeof(var1) << endl;
 
-        //TODO 1
-        cout << "Size of var1 variable: ";
+  cout << "Size of var2 variable: ";
+  cout << sizeof(var2) << endl;
 
-        //TODO 1
-        cout << "Size of var2 variable: ";
+  cout << "Address of var1 variable: ";
+  cout << &var1 << endl;
 
-        //TODO 1
-        cout << "Address of var1 variable: ";
+  cout << "Address of var2 variable: ";
+  cout << &var2 << endl;
 
-        //TODO 1
-        cout << "Address of var2 variable: ";
-
-    }
+}
 }
 
 namespace example2 {
-    void example() {
-        //pointers basic 2
-        //TODO 2 - create a integer variable var holding some value
-        //TODO 2 - create a pointer of type int
+void example() {
+  //pointers basic 2
+  int var = 20;   // actual variable declaration.
+  int *ip;        // pointer variable
 
-        //TODO 2 - store address of var into pointer
+  ip = &var;       // store address of var in pointer variable
 
-        //TODO 2
-        cout << "Value of var variable: ";
+  cout << "Value of var variable: ";
+  cout << var << endl;
 
-        //TODO 2 // print the address stored in ip pointer variable
-        cout << "Address stored in ip variable: ";
+  // print the address stored in ip pointer variable
+  cout << "Address stored in ip variable: ";
+  cout << ip << endl;
 
-        //TODO 2// access the value at the address available in pointer
-        cout << "Value of *ip variable: ";
+  // access the value at the address available in pointer
+  cout << "Value of *ip variable: ";
+  cout << *ip << endl;
 
-        //TODO 2
-        cout << "Address of *ip variable: ";
+  cout << "Address of *ip variable: ";
+  cout << &ip << endl;
 
-        //TODO 2 - set the pointer to null pointer
+  int *ptr = nullptr;
 
-        cout << "The value of ptr is ";
-    }
+  cout << "The value of ptr is " << ptr;
+}
 }
 
 namespace example3 {
-    void example() {
-        //pointer addition
+void example() {
 
-        //TODO 3 - integer array of size MAX
-        //TODO 3 - integer pointer
+  //pointer addition
+  int var[MAX] = {10, 100, 200};
+  int *ptr;
 
-        // let us have array address in pointer.
+  // let us have array address in pointer.
+  ptr = var;
+  for (int i = 0; i < MAX; i++) {
+    cout << "Address of var[" << i << "] = ";
+    cout << ptr << endl;
 
-        //TODO 3 - point pointer to variable
-        for (int i = 0; i < MAX; i++) {
-            //TODO 3
-            cout << "Address of var[" << i << "] = ";
+    cout << "Value of var[" << i << "] = ";
+    cout << *ptr << endl;
 
-            //TODO 3
-            cout << "Value of var[" << i << "] = ";
-
-            //TODO 3// point to the next location
-        }
-    }
+    // point to the next location
+    ptr++;
+  }
+}
 }
 
 namespace example4 {
-    void example() {
+void example() {
 
-        //pointer subtraction
+  //pointer subtraction
+  int var[MAX] = {10, 100, 200};
+  int *ptr;
 
-        //TODO 4 - integer array of size MAX
-        //TODO 4 - integer pointer
+  // let us have address of the last element in pointer.
+  ptr = &var[MAX - 1];
+  for (int i = MAX; i > 0; i--) {
+    cout << "Address of var[" << i << "] = ";
+    cout << ptr << endl;
 
-        //TODO 4// let us have address of the last element in pointer.
+    cout << "Value of var[" << i << "] = ";
+    cout << *ptr << endl;
 
-        for (int i = MAX; i > 0; i--) {
-            //TODO 4
-            cout << "Address of var[" << i << "] = ";
-
-            //TODO 4
-            cout << "Value of var[" << i << "] = ";
-
-            //TODO 4
-            // point to the previous location
-        }
-    }
+    // point to the previous location
+    ptr--;
+  }
+}
 }
 
 namespace example5 {
-    void example() {
+void example() {
 
-        //comparing pointers
+  //comparing pointers
 
-        //TODO 5 - integer array of size MAX
-        //TODO 5 - integer pointer
+  int var[MAX] = {10, 100, 200};
+  int *ptr;
 
-        //TODO 5// let us have address of the first element in pointer.
-        int i = 0;
-        //TODO 5 iterate from &variable[MAX-1] to ptr
-        while (42) {
+  // let us have address of the first element in pointer.
+  ptr = var;
+  int i = 0;
+  while (ptr <= &var[MAX - 1]) {
+    cout << "Address of var[" << i << "] = ";
+    cout << ptr << endl;
 
-            //TODO 5
-            cout << "Address of var[" << i << "] = ";
+    cout << "Value of var[" << i << "] = ";
+    cout << *ptr << endl;
 
-            //TODO 5
-            cout << "Value of var[" << i << "] = ";
-
-            //TODO 5// point to the next location
-
-            i++;
-        }
-    }
+    // point to the previous location
+    ptr++;
+    i++;
+  }
+}
 }
 
 namespace example6 {
-    void example() {
+void example() {
 
-        //incorrect pointers
-        int var[MAX] = {10, 100, 200};
+  //incorrect pointers
+  int var[MAX] = {10, 100, 200};
 
-        for (int i = 0; i < MAX; i++) {
-            *var = i;    // This is a correct syntax
-            //var++;       // This is incorrect.
-        }
-        *(var + 2) = 500;
-    }
+  for (int i = 0; i < MAX; i++) {
+    *var = i;    // This is a correct syntax
+    //var++;       // This is incorrect.
+  }
+  *(var + 2) = 500;
+  cout<<*(var+2)<<endl;
+}
 }
 
 namespace example7 {
-    void example() {
-        //simple array revisted
+void example() {
+  //simple array revisted
 
-        int var[MAX] = {10, 100, 200};
+  int var[MAX] = {10, 100, 200};
 
-        for (int i = 0; i < MAX; i++) {
-            cout << "Value of var[" << i << "] = ";
-            cout << var[i] << endl;
-        }
-    }
+  for (int i = 0; i < MAX; i++) {
+    cout << "Value of var[" << i << "] = ";
+    cout << var[i] << endl;
+  }
+}
 }
 
 namespace example8 {
-    void example() {
+void example() {
 
-        //array of pointers
-        int var[MAX] = {10, 100, 200};
-        //TODO 8 define an integer pointer of size MAX
+  //array of pointers
+  int var[MAX] = {10, 100, 200};
+  int *ptr[MAX];
 
-        for (int i = 0; i < MAX; i++) {
-            //TODO 8// assign the address of integer.
-        }
-        for (int i = 0; i < MAX; i++) {
-            //TODO 8
-            cout << "Value of var[" << i << "] = ";
-        }
-    }
+  for (int i = 0; i < MAX; i++) {
+    ptr[i] = &var[i]; // assign the address of integer.
+  }
+  for (int i = 0; i < MAX; i++) {
+    cout << "Value of var[" << i << "] = ";
+    cout << *ptr[i] << endl;
+  }
+}
 }
 
 namespace example9 {
-    void example() {
+void example() {
 
-        //array of names
+  //array of names
 
-        //TODO 9 - create a character pointer array of size MAX
+  char *names[MAX] = {
+      "Jack A",
+      "Jill B",
+      "John C",
+  };
 
-        for (int i = 0; i < MAX; i++) {
-            //TODO 9
-            cout << "Value of names[" << i << "] = ";
-
-        }
-    }
+  for (int i = 0; i < MAX; i++) {
+    cout << "Value of names[" << i << "] = ";
+    cout << names[i] << endl;
+  }
+}
 }
 
 namespace example10 {
-    void example() {
-        int var;
-        int *ptr;
-        //TODO 10 - create a pointer to pointer
+void example() {
+  int var;
+  int *ptr;
+  int **pptr;
+  int ***ppptr;
 
+  var = 3000;
 
-        var = 3000;
+  // take the address of var
+  ptr = &var;
 
-        // take the address of var
-        ptr = &var;
+  // take the address of ptr using address of operator &
+  pptr = &ptr;
 
-        //TODO 10// take the address of ptr using address of operator &
+  ppptr = &pptr;
 
-        // take the value using pptr
-        cout << "Value of var :" << var << endl;
-        cout << "Value available at *ptr :" << *ptr << endl;
-        //TODO 10
-        cout << "Value available at **pptr :";
+  // take the value using pptr
+  cout << "Value of var :" << var << endl;
+  cout << "Value available at *ptr :" << *ptr << endl;
+  cout << "Value available at **pptr :" << **pptr << endl;
 
-    }
+}
 }
 
 namespace example11 {
-    //passing pointers to functions
-    void getSeconds(unsigned long *par);
+//passing pointers to functions
+void getSeconds(unsigned long *par);
 
+void getSeconds(unsigned long *par) {
+  // get the current number of seconds
+  *par = time(NULL);
+  return;
+}
 
-    void getSeconds(unsigned long *par) {
-        // get the current number of seconds
-        *par = time(NULL);
-        return;
-    }
+void example() {
+  unsigned long sec;
 
-    void example() {
-        unsigned long sec;
+  getSeconds(&sec);
 
+  // print the actual value
+  cout << "Number of seconds :" << sec << endl;
 
-        getSeconds(&sec);
-
-        // print the actual value
-        cout << "Number of seconds :" << sec << endl;
-
-    }
+}
 }
 
 namespace example12 {
-    //More passing pointers to functions
-    double getAverage(int *arr, int size);
+//More pointers to functions
+double getAverage(int *arr, int size);
 
+double getAverage(int *arr, int size) {
+  int i, sum = 0;
+  double avg;
 
-    double getAverage(int *arr, int size) {
-        int i, sum = 0;
-        double avg;
+  for (i = 0; i < size; ++i) {
+    sum += arr[i];
+  }
 
-        for (i = 0; i < size; ++i) {
-            sum += arr[i];
-        }
+  avg = double(sum) / size;
 
-        avg = double(sum) / size;
+  return avg;
+}
 
-        return avg;
-    }
+void example() {
 
-    void example() {
+  // an int array with 5 elements.
+  int balance[5] = {1000, 2, 3, 17, 50};
+  double avg;
 
-        // an int array with 5 elements.
-        int balance[5] = {1000, 2, 3, 17, 50};
-        double avg;
+  // pass pointer to the array as an argument.
+  avg = getAverage(balance, 5);
 
-        // pass pointer to the array as an argument.
-        avg = getAverage(balance, 5);
+  // output the returned value
+  cout << "Average value is: " << avg << endl;
 
-        // output the returned value
-        cout << "Average value is: " << avg << endl;
-
-    }
+}
 }
 
 namespace example13 {
-    //returning pointers from function
+//return pointer to function
 
-    // function to generate and retrun random numbers.
-    int *getRandom() {
-        static int r[10];
+// function to generate and retrun random numbers.
+int *getRandom() {
+  static int r[10];
 
-        // set the seed
-        srand((unsigned) time(NULL));
-        for (int i = 0; i < 10; ++i) {
-            r[i] = rand();
-            cout << r[i] << endl;
-        }
+  // set the seed
+  srand((unsigned) time(NULL));
+  for (int i = 0; i < 10; ++i) {
+    r[i] = rand();
+    cout << r[i] << endl;
+  }
 
-        return r;
-    }
-
-    // function to call above defined function.
-
-    void example() {
-        // a pointer to an int.
-        int *p;
-
-        p = getRandom();
-        for (int i = 0; i < 10; i++) {
-            cout << "*(p + " << i << ") : ";
-            cout << *(p + i) << endl;
-        }
-
-
-    }
+  return r;
 }
 
+// function to call above defined function.
+
+void example() {
+  // a pointer to an int.
+  int *p;
+
+  p = getRandom();
+  for (int i = 0; i < 10; i++) {
+    cout << "*(p + " << i << ") : ";
+    cout << *(p + i) << endl;
+  }
+
+}
+}
 
 int main() {
 
-    //example1::example();
-    //example2::example();
-    //example3::example();
-    //example4::example();
-    //example5::example();
-    //example6::example();
-    //example7::example();
-    //example8::example();
-    //example9::example();
-    //example10::example();
-    //example11::example();
-    //example12::example();
-    //example13::example();
+  example1::example();
+  //example2::example();
+  //example3::example();
+  //example4::example();
+  //example5::example();
+  //example6::example();
+  //example10::example();
+  //example10::example();
+  //example11::example();
+  //example12::example();
+  //example13::example();
 
 
-    return 0;
+  return 0;
 }
